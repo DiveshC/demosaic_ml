fe1164fa0e1a512b179f32106d1b6f61d889050c
diveshc <chudasad@mcmaster.ca>
2021-04-04 00:38:54 -0400

    able to get weights for ABC patches, D not working yet

--- train.py
+++ train.py
@@ -22,7 +22,7 @@ img = Image.open(input_filename)
 img_data_rgb = np.asarray(img)
 
 ## get mosaic of image
-img_data = mosaic(img_data_rgb.)
+img_data = mosaic(img_data_rgb.T)
 
 ### PATCH TYPES GB, RB, RB_2, RG
 
@@ -36,6 +36,10 @@ RG = np.array([[]])
 input_RG = np.array([[]])
 
 ### Pull patches into input Training sets and rgb values into output training sets
+# print(img_data_rgb.T[0].T.shape)
+R = img_data_rgb.T[0].T
+G = img_data_rgb.T[1].T
+B = img_data_rgb.T[2].T
 
 for x in range(0, img_data.shape[0], 5):
     for y in range(0, img_data.shape[0], 5):
@@ -43,67 +47,74 @@ for x in range(0, img_data.shape[0], 5):
         if(x == 0):
             block = img_data[:5, y:y+5]
             col = mat_to_col(block)
+            g = G[x+2][y+2]
+            b = B[x+2][y+2]
             if(input_GB.shape == (1,0)):
                 input_GB = col
+                GB = [[g, b]]
             else:
                 input_GB = np.append(input_GB, col, axis = 0)
-            g = img_data_rgb[1][x+2][y+2]
-            b = img_data_rgb[2][x+2][y+2]
-            GB = np.append(GB, [[g, b]], axis = 0)
+                GB = np.append(GB, [[g, b]], axis = 0)
         # RB PATCH
         elif((x/5)%2 == 1):
             block = img_data[x:x+5, y:y+5]
             col = mat_to_col(block)
+            r = R[x+2][y+2]
+            b = B[x+2][y+2]
             if(input_RB.shape == (1,0)):
                 input_RB = col
+                RB = [[r, b]]
             else:
                 input_RB = np.append(input_RB, col, axis = 0)
-            r = img_data_rgb[0][x+2][y+2]
-            b = img_data_rgb[2][x+2][y+2]
-            RB = np.append(RB, [[r, b]], axis = 0)
+                RB = np.append(RB, [[r, b]], axis = 0)
         # RB_2 PATCH
         elif((x/5)%2 == 0):
             block = img_data[x:x+5, y:y+5]
             col = mat_to_col(block)
+            r = R[x+2][y+2]
+            b = B[x+2][y+2]
             if(input_RB_2.shape == (1,0)):
-                input_GB = col
+                input_RB_2 = col
+                RB_2 = [[r, b]]
             else:
                 input_RB_2 = np.append(input_RB_2, col, axis = 0)
-            r = img_data_rgb[0][x+2][y+2]
-            b = img_data_rgb[2][x+2][y+2]
-            RB_2 = np.append(RB_2, [[r, b]], axis = 0)
+                RB_2 = np.append(RB_2, [[r, b]], axis = 0)
         # RG PATCH
+        # elif(x == img_data.shape[0]-5):
         else:
             block = img_data[x:x+5, y:y+5]
             col = mat_to_col(block)
+            r = R[x+2][y+2]
+            g = G[x+2][y+2]
             if(input_RG.shape == (1,0)):
                 input_RG = col
+                RG = [[r, g]]
             else:
                 input_RG = np.append(input_RG, col, axis = 0)
-            r = img_data_rgb[0][x+2][y+2]
-            g = img_data_rgb[1][x+2][y+2]
-            RG = np.append(RG, [[r, g]], axis = 0)
+                RG = np.append(RG, [[r, g]], axis = 0)
 
 
 
 ### Compute Coefficients and Store in txt file
 f = open("weights.txt", "a")
-G_A = gen_weights(input_GB, GB[0])
-f.write(G_A)
-B_A = gen_weights(input_GB, GB[1])
-f.write(B_A)
-R_A = gen_weights(input_RB, RB[0])
-f.write(R_A)
-B_A = gen_weights(input_RB, RB[1])
-f.write(B_A)
-R_A = gen_weights(input_RB_2, RB_2[0])
-f.write(R_A)
-B_A = gen_weights(input_RB_2, RB_2[1])
-f.write(B_A)
-R_A = gen_weights(input_RG, RG[0])
-f.write(R_A)
-G_A = gen_weights(input_RG, RG[0])
-f.write(G_A)
+## TYPE A GB
+# print(GB.T, GB)
+G_coeff, B_coeff = gen_weights(input_GB, GB.T)
+f.write(np.array2string(G_coeff))
+f.write(np.array2string(B_coeff))
+## TYPE B RB
+R_coeff, B_coeff = gen_weights(input_RB, RB.T)
+f.write(np.array2string(R_coeff))
+f.write(np.array2string(B_coeff))
+## TYPE C RB
+R_coeff, B_coeff = gen_weights(input_RB_2, RB_2.T)
+f.write(np.array2string(R_coeff))
+f.write(np.array2string(B_coeff))
+# TYPE D RG
+print(input_RG, RG)
+R_coeff, G_coeff = gen_weights(input_RG, RG.T)
+f.write(np.array2string(R_coeff))
+f.write(np.array2string(G_coeff))
 
 f.close()
 
--- utils/regression.py
+++ utils/regression.py
@@ -12,11 +12,15 @@ def mat_to_col(inp):
         for y in range(mat_Y):
             out[0][inc] = inp[x][y]
             inc+=1
+
+    # print(out)
     return out
 
 def gen_weights(X, Y):
     print('Input X dimensions: {} Output Y dimensions: {}'.format(X.shape,Y.shape))
     x_inv = inv(matmul(X.T, X))
     x_plus = matmul(x_inv, X.T)
-    A = matmul(x_plus, Y)
     
+    A = matmul(x_plus, Y[0])
+    B = matmul(x_plus, Y[1])
+    return A,B
